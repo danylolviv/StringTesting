a923ae0bafe29b011459172ab59ae1b23d6eb4c4
Danylo <danyloboshagora@gmail.com>
2020-09-24 00:44:18 +0200

    split characters and output them with @ in front

--- src/com/ButWhy/Main.java
+++ src/com/ButWhy/Main.java
@@ -5,11 +5,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Dev Branch");
+        System.out.println("Splitting string into char");
 
         Scanner scanner = new Scanner(System.in);
         String output = scanner.nextLine();
-        System.out.println();
+
+
+        for (int i=0; i< output.length(); i++) {
+            char c = output.charAt(i);
+            System.out.println("@"+ c);
+        }
+
         scanner.close();
     }
 }
