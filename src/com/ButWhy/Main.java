16e4ef2336062e64ca313b0d9531734f59a771c8
Danylo <danyloboshagora@gmail.com>
2020-09-24 00:52:46 +0200

    reversed the string from input

--- src/com/ButWhy/Main.java
+++ src/com/ButWhy/Main.java
@@ -7,9 +7,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Dev Branch");
 
+
         Scanner scanner = new Scanner(System.in);
         String output = scanner.nextLine();
-        System.out.println();
+        String reverse = new StringBuffer(output).reverse().toString();
+        System.out.println(reverse);
         scanner.close();
     }
 }
