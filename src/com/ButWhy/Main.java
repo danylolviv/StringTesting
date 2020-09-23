028a253528bf316da863af361592f842182c0784
Danylo <danyloboshagora@gmail.com>
2020-09-23 21:14:56 +0200

    adde the beginning of the new branch

--- src/com/ButWhy/Main.java
+++ src/com/ButWhy/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("First Hello world");
+        System.out.println("Initiating new dev branch");
     }
 }
