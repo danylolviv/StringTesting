10945f8409bc7b8404f3d37bb68796464f3d3fcd
Danylo <danyloboshagora@gmail.com>
2020-09-23 21:57:05 +0200

    finished with the scanner

--- src/com/ButWhy/Main.java
+++ src/com/ButWhy/Main.java
@@ -1,9 +1,16 @@
 package com.ButWhy;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("First Hello world");
         System.out.println("Initiating new dev branch");
+
+        Scanner scanner = new Scanner(System.in);
+        String bruhName = scanner.nextLine();
+        System.out.println("and the name of the bruh is:" + bruhName);
+        scanner.close();
     }
 }
